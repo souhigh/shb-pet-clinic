883dab5579702f29678d77983aef99c3abf88fe3
Souhaib driouch <deprugs@gmail.com>
2019-01-28 21:48:51 +0100

    Initial Project Add

---
+++ src/main/java/sohaib/springframework/shbpetclinic/ShbPetClinicApplication.java
@@ -0,0 +1,14 @@
+package sohaib.springframework.shbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShbPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/sohaib/springframework/shbpetclinic/ShbPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package sohaib.springframework.shbpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShbPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
