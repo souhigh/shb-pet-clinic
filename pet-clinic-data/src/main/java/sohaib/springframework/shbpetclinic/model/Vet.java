2a05b9a4b4133a5cd39476ea761ddc8559241f2f
Souhaib driouch <deprugs@gmail.com>
2019-02-17 19:37:10 +0100

    added Speciality, added to vet. Closes #28

--- pet-clinic-data/src/main/java/sohaib/springframework/shbpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/sohaib/springframework/shbpetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package sohaib.springframework.shbpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialties;
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
+    }
 }
