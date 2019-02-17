7ad1cb161b92b7e0506f536d5375b79a3e0fa3bf
Souhaib driouch <deprugs@gmail.com>
2019-02-17 21:38:34 +0100

    added PetType Map Service Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/sohaib/springframework/shbpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package sohaib.springframework.shbpetclinic.services;
+
+import sohaib.springframework.shbpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
