dd03398b6f432d2caff83f95291405c9fdb908ad
Souhaib driouch <deprugs@gmail.com>
2019-02-18 14:08:27 +0100

    Added Speciality Map Service Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/sohaib/springframework/shbpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package sohaib.springframework.shbpetclinic.services;
+
+import sohaib.springframework.shbpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
